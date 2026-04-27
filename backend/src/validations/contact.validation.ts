c74a5d07cde5feb629a6e6f0e2c510208ee06989
Demba Faye <fayecodeur99@gmail.com>
2026-04-27 17:54:59 +0000

    Création des regles de validations des données

---
+++ backend/src/validations/contact.validation.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const contactSchema = z.object({
+  name: z.string().min(1, "Le nom est obligatoire"),
+  email: z.string().email("L''email est invalid"),
+  phone: z.string().min(6, "Téléphone invalide"),
+});
