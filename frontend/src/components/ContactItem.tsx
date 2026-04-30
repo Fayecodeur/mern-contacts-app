f594bd8bcbdc585e66ddd8d4c7b756507c3d048c
Demba Faye <fayecodeur99@gmail.com>
2026-04-30 19:13:18 +0000

    Décomposition du composant

--- frontend/src/components/ContactItem.tsx
+++ frontend/src/components/ContactItem.tsx
@@ -1,3 +1,19 @@
-export default function ContactItem() {
-  return <div>ContactItem</div>;
+import type { Contact } from "../types/contact";
+
+type PropItems = {
+  contact: Contact;
+};
+export default function ContactItem({ contact }: PropItems) {
+  const { name, email, phone } = contact;
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{email}</td>
+      <td>{phone}</td>
+      <td>
+        <button className="btn btn-warning btn-sm me-2">Edit</button>
+        <button className="btn btn-danger btn-sm">Delete</button>
+      </td>
+    </tr>
+  );
 }
