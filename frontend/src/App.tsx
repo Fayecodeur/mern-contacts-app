d02e41684e5e8e81d0210d95eedd9aa6b42a9796
Demba Faye <fayecodeur99@gmail.com>
2026-05-01 19:17:19 +0000

    Gestion de la navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import ContactsList from "./pages/ContactsList";
+import AddContact from "./pages/AddContact";
 
 function App() {
   return (
     <>
-      <ContactsList />
+      <Routes>
+        <Route path="/" element={<ContactsList />} />
+        <Route path="/create" element={<AddContact />} />
+      </Routes>
     </>
   );
 }
